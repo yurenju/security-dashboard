6d0106f9a73720da8dc61ea46dfccd1130115f48
Yuren Ju <yurenju@gmail.com>
2017-08-25 09:13:21 +0800

    Add index as property since it is required field

--- src/Record.test.js
+++ src/Record.test.js
@@ -7,14 +7,14 @@ import Record from './Record';
 it('properties of event should be rendered correctly', () => {
   const event = { prediction: 'car' };
   const onRead = function() {};
-  const record = shallow(<Record event={event} onRead={onRead} />);
+  const record = shallow(<Record index={0} event={event} onRead={onRead} />);
   expect(record.find('.prediction span').text()).to.equal('car');
 });
 
 it('event_id and camera_id should only appear when record is expanded', () => {
   const event = { prediction: 'car' };
   const onRead = function () { };
-  const record = shallow(<Record event={event} onRead={onRead} />);
+  const record = shallow(<Record index={0} event={event} onRead={onRead} />);
   expect(record.find('.event-id')).to.have.length(0);
   expect(record.find('.camera-id')).to.have.length(0);
   record.instance().onClick();
@@ -25,7 +25,7 @@ it('event_id and camera_id should only appear when record is expanded', () => {
 it('onRead should only be call when event is unread', () => {
   const event = { unread: false };
   const onRead = sinon.spy();
-  const record = shallow(<Record event={event} onRead={onRead} />);
+  const record = shallow(<Record index={0} event={event} onRead={onRead} />);
   record.instance().onClick();
   expect(onRead).to.have.property('callCount', 0);
   event.unread = true;
